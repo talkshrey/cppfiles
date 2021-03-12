b1bbe6274448ef17be045fc256413b0cd67817cd
talkshrey <74039736+talkshrey@users.noreply.github.com>
2021-03-12 22:10:32 +0530

    Create basics.cpp

---
+++ basics.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a = 10;
+  string s = "shrey"; // declare
+  cout << sizeof(char);
+  cout << sizeof(int);
+  cout << sizeof(float);
+  cout << sizeof(double);
+  cout << a++;
+  cout << length(s);
+  return 0;
